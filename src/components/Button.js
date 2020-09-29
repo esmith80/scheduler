117caa2e5b2659fb28d7d16a9abf8a749b787bb5
11em <Edward@Edwards-iMac.local>
2020-09-28 21:04:08 -0600

    first full day of building scheduler with dayList functionality and tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
-
 import "components/Button.scss";
+const classNames = require('classnames');
+
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames(
+      // required classes
+      "button",
+      { // conditional classes
+        "button--confirm": props.confirm,
+        "button--danger" : props.danger
+      }
+   );
+
+   return ( 
+     <button 
+      className={buttonClass}
+      disabled={props.disabled}
+      onClick={props.onClick}
+     >
+      {props.children}
+     </button>
+   );
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import 'components/DayListItem.scss';
+const classNames = require('classnames');
+
+
+const formatSpots = (spots) => {
+  if (!spots) {
+    return "no spots remaining";
+  } else if (spots > 1) {
+    return `${spots} spots remaining`;
+  } else {
+    return '1 spot remaining'
+  }
+};
+
+export default function DayListItem(props) {
+  const dayListItemClass = classNames('day-list__item',
+    { 
+      "day-list__item--selected": props.selected,
+      "day-list__item--full":     !props.spots  
+    }
+  );
+
+  return (
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+      <h2 >{props.name}</h2> 
+      <h3 >{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
